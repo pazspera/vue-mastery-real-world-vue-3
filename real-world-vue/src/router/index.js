d1ae1753a9d0ce503760a9fd04bc7916ef6a6482
Paz Spera <mspera03@gmail.com>
2022-07-23 12:34:43 -0300

    segunda parte video 06 dynamic routing, hacer las rutas dinámicas

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -15,8 +15,9 @@ const routes = [
     component: AboutView,
   },
   {
-    path: "/event/123",
+    path: "/event/:id",
     name: "EventDetails",
+    props: true,
     component: EventDetails,
   },
 ];
